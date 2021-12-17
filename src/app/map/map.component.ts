0758024e39ad822b201cd60319982e4cff1801bc
tesla <kela256@gmail.com>
2021-12-17 10:02:02 +0200

    change map component

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-map',
@@ -6,18 +6,14 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./map.component.scss'],
 })
 export class MapComponent {
-
+    
     public zoom = 15;
-
+    
     public position = {
         lat: 50.4638308,
         lng: 30.5006951,
     };
-
+    
     constructor() {
     }
-
-    //in index html add script 
-    //<script src="https://maps.googleapis.com/maps/api/js?key=KEY&callback=initMap" ></script>
-    
 }
